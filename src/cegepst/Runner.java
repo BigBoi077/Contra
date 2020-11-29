0fada047f697e5313baadca52d1a2d53e286b1b7
BigBoi077 <58580199+BigBoi077@users.noreply.github.com>
2020-11-28 19:49:31 -0500

    Fixed AliensSprites.png

---
+++ src/cegepst/Runner.java
@@ -0,0 +1,40 @@
+package cegepst;
+
+import cegepst.engine.Buffer;
+
+public class Runner extends Alien {
+    @Override
+    public void initSpritesheet() {
+
+    }
+
+    @Override
+    public void initFrames() {
+
+    }
+
+    @Override
+    public void readSprites() {
+
+    }
+
+    @Override
+    public void cycleFrames() {
+
+    }
+
+    @Override
+    public boolean nearPlayer() {
+        return false;
+    }
+
+    @Override
+    public void spawn() {
+
+    }
+
+    @Override
+    public void draw(Buffer buffer) {
+
+    }
+}
